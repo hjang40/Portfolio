317c59eeb82d7cd502511584630f0babc58946b9
Hyun Seo Jang <hjang40@gmail.com>
2025-09-18 20:51:20 -0400

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Route, BrowserRouter as Router, Routes} from 'react-router-dom'
+import {Route, HashRouter as Router, Routes} from 'react-router-dom'
 import Navbar from './components/Navbar'
 import {Home, About, Projects, Contact} from './pages'
 
